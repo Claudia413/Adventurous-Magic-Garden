50c7fb7e627166d15947d0c4522b44ceb23f33bc
Claudia <cmjengelsman@gmail.com>
2021-11-13 18:48:04 +1300

    Run linter

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,53 +7,55 @@
     </div>
     <p>
       Made with love by
-      <a href="https://www.claudiaengelsman.com/" rel="noopener" class="credit">Claudia Engelsman</a>
+      <a href="https://www.claudiaengelsman.com/" rel="noopener" class="credit"
+        >Claudia Engelsman</a
+      >
     </p>
   </footer>
 </template>
 
 <script>
-  export default {
-    name: "FooterComponent",
-    components: {},
-  };
+export default {
+  name: "FooterComponent",
+  components: {},
+};
 </script>
 
 <style scoped lang="scss">
-  footer {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    color: rgba(0, 0, 0, 1);
-    &:hover {
-      .icon {
-        color: rgba(0, 0, 0, 0.35);
-        &:hover {
-          color: rgba(0, 0, 0, 1);
-        }
+footer {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  color: rgba(0, 0, 0, 1);
+  &:hover {
+    .icon {
+      color: rgba(0, 0, 0, 0.35);
+      &:hover {
+        color: rgba(0, 0, 0, 1);
       }
     }
-    .icons {
-      display: flex;
-      justify-content: center;
-      margin: 40px 0;
-    }
-    .icon {
-      margin: 4px;
-      color: rgba(0, 0, 0, 1);
-      transition: all 0.25s ease-in-out;
-    }
-    p {
-      font-family: "Josefin Sans", Arial, sans-serif;
-      color: rgba(0, 0, 0, 0.75);
-      text-align: center;
-      font-size: 12px;
-      margin: 48px;
-    }
-    .credit {
-      text-decoration: none;
-      color: inherit;
-      cursor: pointer;
-    }
   }
+  .icons {
+    display: flex;
+    justify-content: center;
+    margin: 40px 0;
+  }
+  .icon {
+    margin: 4px;
+    color: rgba(0, 0, 0, 1);
+    transition: all 0.25s ease-in-out;
+  }
+  p {
+    font-family: "Josefin Sans", Arial, sans-serif;
+    color: rgba(0, 0, 0, 0.75);
+    text-align: center;
+    font-size: 12px;
+    margin: 48px;
+  }
+  .credit {
+    text-decoration: none;
+    color: inherit;
+    cursor: pointer;
+  }
+}
 </style>
--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -25,11 +25,20 @@
           </template>
           <template v-else-if="slice.slice_type === 'picture'">
             <prismic-image :field="slice.primary.picture" class="blog-image" />
-            <prismic-rich-text :field="slice.primary.caption" class="blog-image-caption" />
+            <prismic-rich-text
+              :field="slice.primary.caption"
+              class="blog-image-caption"
+            />
           </template>
           <template v-else-if="slice.slice_type === 'text_with_image'">
-            <prismic-image :field="slice.primary.image" class="blog-image-next-text" />
-            <prismic-rich-text :field="slice.primary.text" class="blog-text-next-image" />
+            <prismic-image
+              :field="slice.primary.image"
+              class="blog-image-next-text"
+            />
+            <prismic-rich-text
+              :field="slice.primary.text"
+              class="blog-text-next-image"
+            />
           </template>
           <template v-else-if="slice.slice_type === 'shoot_stats'">
             <span class="shoot-stat">
@@ -80,197 +89,199 @@
 </template>
 
 <script>
-  import moment from "moment";
-  import NavigationComponent from "@/components/Navigation.vue";
+import moment from "moment";
+import NavigationComponent from "@/components/Navigation.vue";
 
-  export default {
-    name: "BlogPost",
-    data() {
-      return {
-        blog: {
-          title: [{ text: "" }],
-          labels: [],
-          prismicID: "",
-          uid: "",
-          first_publication_date: "",
-          seo_title: "",
-          seo_description: "",
-          seo_image: "",
-        },
-        slices: [],
-        slides: [],
-      };
-    },
-    components: {
-      NavigationComponent,
-    },
-    methods: {
-      getContent(uid) {
-        this.$prismic.client.getByUID("blogpost", uid).then((document) => {
-          console.log("what do we get back", document);
-          // Can't simply put the result in an empty blog object without console erros about undefined before its done loading. So we set them individually with starting empty objects and arrays
-          this.blog.title = document.data.blog_title;
-          this.blog.first_publication_date = moment(document.first_publication_date).format("MMMM Do YYYY");
-          this.blog.labels = document.tags;
-          this.blog.seo_title = document.data.seo_title[0].text;
-          this.blog.seo_description = document.data.seo_description[0].text;
-          this.blog.seo_image = document.data.seo_image.url;
-          this.slices = document.data.body;
-          this.blog.prismicID = document.id;
-          var that = this;
-          this.slices.forEach(function (slice) {
-            // find carousel slice if present and create data the carousel plugin will accept
-            if (slice.slice_type === "carousel") {
-              that.slides = slice.items;
-            }
-          });
-        });
+export default {
+  name: "BlogPost",
+  data() {
+    return {
+      blog: {
+        title: [{ text: "" }],
+        labels: [],
+        prismicID: "",
+        uid: "",
+        first_publication_date: "",
+        seo_title: "",
+        seo_description: "",
+        seo_image: "",
       },
+      slices: [],
+      slides: [],
+    };
+  },
+  components: {
+    NavigationComponent,
+  },
+  methods: {
+    getContent(uid) {
+      this.$prismic.client.getByUID("blogpost", uid).then((document) => {
+        console.log("what do we get back", document);
+        // Can't simply put the result in an empty blog object without console erros about undefined before its done loading. So we set them individually with starting empty objects and arrays
+        this.blog.title = document.data.blog_title;
+        this.blog.first_publication_date = moment(
+          document.first_publication_date
+        ).format("MMMM Do YYYY");
+        this.blog.labels = document.tags;
+        this.blog.seo_title = document.data.seo_title[0].text;
+        this.blog.seo_description = document.data.seo_description[0].text;
+        this.blog.seo_image = document.data.seo_image.url;
+        this.slices = document.data.body;
+        this.blog.prismicID = document.id;
+        var that = this;
+        this.slices.forEach(function (slice) {
+          // find carousel slice if present and create data the carousel plugin will accept
+          if (slice.slice_type === "carousel") {
+            that.slides = slice.items;
+          }
+        });
+      });
     },
-    created() {
-      this.getContent(this.$route.params.uid);
-      this.blog.uid = this.$route.params.uid;
-    },
-    beforeRouteUpdate(to, from, next) {
-      //   This prevents blog page not updating upon route change
-      this.getContent(to.params.uid);
-      next();
-    },
-  };
+  },
+  created() {
+    this.getContent(this.$route.params.uid);
+    this.blog.uid = this.$route.params.uid;
+  },
+  beforeRouteUpdate(to, from, next) {
+    //   This prevents blog page not updating upon route change
+    this.getContent(to.params.uid);
+    next();
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .blog {
-    padding: 80px;
-    display: flex;
-    justify-content: center;
-  }
+.blog {
+  padding: 80px;
+  display: flex;
+  justify-content: center;
+}
 
-  article {
-    max-width: 1200px;
-  }
+article {
+  max-width: 1200px;
+}
 
-  .title,
-  .publish-date {
-    text-align: center;
-    font-family: "Baskervville", Georgia, "Times New Roman", Times, serif;
-  }
+.title,
+.publish-date {
+  text-align: center;
+  font-family: "Baskervville", Georgia, "Times New Roman", Times, serif;
+}
 
-  .title {
-    font-weight: 400;
-    font-size: 42px;
-    line-height: 50px;
-  }
+.title {
+  font-weight: 400;
+  font-size: 42px;
+  line-height: 50px;
+}
 
-  .publish-date {
-    color: RGBA(20, 20, 20, 0.5);
+.publish-date {
+  color: RGBA(20, 20, 20, 0.5);
+  font-size: 13px;
+  font-style: italic;
+}
+.picture {
+  display: flex;
+  flex-direction: column;
+  padding: 0 10%;
+}
+.blog-image {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  margin-top: 20px;
+}
+
+.text {
+  ul,
+  ol {
+    font-size: 16px;
+  }
+}
+
+.blog-image-caption {
+  font-size: 12px;
+  line-height: 20px;
+  padding-top: 8px;
+  margin-bottom: 20px;
+  p {
+    margin: 0;
+    padding: 0;
+  }
+}
+
+.shoot_stats {
+  display: flex;
+  justify-content: space-between;
+  align-content: space-between;
+  margin: 64px 0;
+}
+
+.shoot-stat {
+  margin: 8px;
+  display: flex;
+  align-items: center;
+  &:nth-child(1) {
+    margin: 8px 0;
+  }
+  .shoot-stat-icon {
+    margin-right: 8px;
+    height: 24px;
+  }
+  @media only screen and (max-width: 768px) {
     font-size: 13px;
-    font-style: italic;
   }
-  .picture {
-    display: flex;
-    flex-direction: column;
-    padding: 0 10%;
-  }
-  .blog-image {
-    width: 100%;
-    height: auto;
-    object-fit: cover;
-    margin-top: 20px;
-  }
+}
 
-  .text {
-    ul,
-    ol {
-      font-size: 16px;
-    }
-  }
-
-  .blog-image-caption {
-    font-size: 12px;
-    line-height: 20px;
-    padding-top: 8px;
-    margin-bottom: 20px;
-    p {
-      margin: 0;
-      padding: 0;
-    }
-  }
-
-  .shoot_stats {
-    display: flex;
-    justify-content: space-between;
-    align-content: space-between;
-    margin: 64px 0;
-  }
-
-  .shoot-stat {
-    margin: 8px;
-    display: flex;
-    align-items: center;
-    &:nth-child(1) {
-      margin: 8px 0;
-    }
-    .shoot-stat-icon {
-      margin-right: 8px;
-      height: 24px;
-    }
-    @media only screen and (max-width: 768px) {
-      font-size: 13px;
-    }
-  }
-
-  .text_with_image {
-    display: flex;
-    justify-content: space-between;
-    &.image-on-right {
-      flex-direction: row-reverse;
-      @media only screen and (max-width: 768px) {
-        flex-direction: column;
-      }
-    }
-    .blog-image-next-text {
-      margin-top: 20px;
-      max-width: 48%;
-      height: auto;
-      object-fit: cover;
-    }
-    .blog-text-next-image {
-      max-width: 48%;
-      height: auto;
-      margin-top: 20px;
-    }
+.text_with_image {
+  display: flex;
+  justify-content: space-between;
+  &.image-on-right {
+    flex-direction: row-reverse;
     @media only screen and (max-width: 768px) {
       flex-direction: column;
-      .blog-image-next-text {
-        max-width: 80%;
-        margin-left: 10%;
-        margin-bottom: 20px;
-      }
-      .blog-text-next-image {
-        max-width: 100%;
-      }
     }
   }
+  .blog-image-next-text {
+    margin-top: 20px;
+    max-width: 48%;
+    height: auto;
+    object-fit: cover;
+  }
+  .blog-text-next-image {
+    max-width: 48%;
+    height: auto;
+    margin-top: 20px;
+  }
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    .blog-image-next-text {
+      max-width: 80%;
+      margin-left: 10%;
+      margin-bottom: 20px;
+    }
+    .blog-text-next-image {
+      max-width: 100%;
+    }
+  }
+}
 
-  .carousel {
-    margin-top: 40px;
-    .item {
-      width: 100%;
-      height: 500px;
-      max-height: 800px;
-      object-fit: cover;
-      @media only screen and (max-width: 768px) {
-        max-height: 320px;
-      }
-      img {
-        height: 100%;
-      }
+.carousel {
+  margin-top: 40px;
+  .item {
+    width: 100%;
+    height: 500px;
+    max-height: 800px;
+    object-fit: cover;
+    @media only screen and (max-width: 768px) {
+      max-height: 320px;
     }
-    .custom-navigation {
-      background-color: rgba(255, 255, 255, 0.8);
-      border-radius: 50%;
-      cursor: pointer;
+    img {
+      height: 100%;
     }
   }
+  .custom-navigation {
+    background-color: rgba(255, 255, 255, 0.8);
+    border-radius: 50%;
+    cursor: pointer;
+  }
+}
 </style>
